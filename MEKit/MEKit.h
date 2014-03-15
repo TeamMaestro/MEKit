f229371c430d7e09ccb3e92dbc32c1e944897af8
William Towe <willbur1984@gmail.com>
2014-03-15 01:50:55 -0400

    Initial Commit

---
+++ MEKit/MEKit.h
@@ -0,0 +1,13 @@
+//
+//  MEKit.h
+//  MEKit
+//
+//  Created by William Towe on 3/15/14.
+//  Copyright (c) 2014 Maestro, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MEKit : NSObject
+
+@end
---
+++ MEKit/MEKit.m
@@ -0,0 +1,13 @@
+//
+//  MEKit.m
+//  MEKit
+//
+//  Created by William Towe on 3/15/14.
+//  Copyright (c) 2014 Maestro, LLC. All rights reserved.
+//
+
+#import "MEKit.h"
+
+@implementation MEKit
+
+@end
