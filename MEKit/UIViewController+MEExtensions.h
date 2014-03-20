e8ecf1ac2e8591ae9b1b9c1142fad0e218caba6d
William Towe <willbur1984@gmail.com>
2014-03-20 13:42:14 -0400

    bump podspec to 1.0.3

---
+++ MEKit/UIViewController+MEExtensions.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+MEExtensions.h
+//  MEKit
+//
+//  Created by William Towe on 3/20/14.
+//  Copyright (c) 2014 Maestro, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (MEExtensions)
+
+- (UIViewController *)ME_rootViewController;
+
+@end
