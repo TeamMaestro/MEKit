77c6ae49d08a72c27f0f20810c824ad77a487e81
William Towe <willbur1984@gmail.com>
2014-10-03 09:00:46 -0400

    add comments to MEPDFExtensions category header

--- MEKit/UIImage+MEPDFExtensions.h
+++ MEKit/UIImage+MEPDFExtensions.h
@@ -13,20 +13,52 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ Mask that describes the cache options used when storing an image generated from a PDF.
+ 
+ - `MEPDFExtensionsCacheOptionsNone`, images are never cached
+ - `MEPDFExtensionsCacheOptionsFile`, images are cached on disk
+ - `MEPDFExtensionsCacheOptionsMemory`, images are cached in memory
+ - `MEPDFExtensionsCacheOptionsAll`, images are cached on disk and in memory
+ */
 typedef NS_OPTIONS(NSInteger, MEPDFExtensionsCacheOptions) {
     MEPDFExtensionsCacheOptionsNone = 0,
     MEPDFExtensionsCacheOptionsFile = 1 << 0,
     MEPDFExtensionsCacheOptionsMemory = 1 << 1,
-    MEPDFExtensionsCacheOptionsAll = MEPDFExtensionsCacheOptionsFile | MEPDFExtensionsCacheOptionsMemory,
-    MEPDFExtensionsCacheOptionsDefault = MEPDFExtensionsCacheOptionsAll
+    MEPDFExtensionsCacheOptionsAll = MEPDFExtensionsCacheOptionsFile | MEPDFExtensionsCacheOptionsMemory
 };
 
 @interface UIImage (MEPDFExtensions)
 
+/**
+ Returns the current image caching options.
+ */
 + (MEPDFExtensionsCacheOptions)ME_defaultPDFExtensionsCacheOptions;
+/**
+ Sets the current image caching options.
+ 
+ @see MEPDFExtensionsCacheOptions
+ */
 + (void)setME_defaultPDFExtensionsCacheOptions:(MEPDFExtensionsCacheOptions)cacheOptions;
 
+/**
+ Calls `ME_imageWithPDFNamed:inBundle:size:`, passing _pdfName_, `nil`, and _size_ respectively.
+ 
+ @param pdfName The name of the PDF file, optionally including the file extension
+ @param size The desired size of the generated image. The image will be scaled to fit _size_ while maintaining its aspect ratio
+ @return The resulting image
+ @exception NSException Thrown if _pdfName_ is nil or _size_ is equal to `CGSizeZero`
+ */
 + (UIImage *)ME_imageWithPDFNamed:(NSString *)pdfName size:(CGSize)size;
+/**
+ Creates an image from the PDF named _pdfName_ from _bundle_ scaled to fit _size_. Optionally caches the image based on the current cache options.
+ 
+ @param pdfName The name of the PDF file, optionally including the file extension
+ @param bundle The bundle which contains the file named _pdfName_. If nil, defaults to `[NSBundle mainBundle]`
+ @param size The desired size of the generated image. The image will be scaled to fit _size_ while maintaining its aspect ratio
+ @return The resulting image
+ @exception NSException Thrown if _pdfName_ is nil or _size_ is equal to `CGSizeZero`
+ */
 + (UIImage *)ME_imageWithPDFNamed:(NSString *)pdfName inBundle:(NSBundle *)bundle size:(CGSize)size;
 
 @end
